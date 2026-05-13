db9bc4725b489857dc3bcb601a13a734d2d82ddf
Juani <juani221.moragues@gmail.com>
2026-05-13 18:31:28 -0300

    lesson 6 - task 1

---
+++ app/drivers/subsys/our_sensor/include/our_sensor.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <zephyr/device.h>
+#include <zephyr/toolchain.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+    
+
+
+#ifdef __cplusplus
+}
+#endif
--- app/src/main.c
+++ app/src/main.c
@@ -5,10 +5,30 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
+
+#define BLINK_TIME_ms		500
+
+static const struct device *sensor_device = DEVICE_DT_GET_ANY(our_sensor);
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
+	if(!device_is_ready(sensor_device)){
+		return 0;
+	}
+
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	while(1){
+		sensor_sample_fetch(sensor_device);
+		k_msleep(BLINK_TIME_ms);
+		sensor_channel_get(sensor_device,0,NULL);
+		k_msleep(BLINK_TIME_ms);
+	}
 
 	return 0;
 }
