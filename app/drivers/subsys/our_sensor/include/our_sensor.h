5ebaef65d643c3b2df6a78a63f0acfc6abe1c164
Juani <juani221.moragues@gmail.com>
2026-05-16 19:53:05 -0300

    Lesson 7 - Task 1

--- app/drivers/subsys/our_sensor/include/our_sensor.h
+++ app/drivers/subsys/our_sensor/include/our_sensor.h
@@ -7,14 +7,6 @@
 extern "C"{
 #endif
     
-/* custom_sensor.h */
-typedef int (*custom_extension_api_t)(const struct device *dev, uint32_t new_val);
-
-struct custom_sensor_api_extension {
-    struct sensor_driver_api common; // Keep the standard one first
-    custom_extension_api_t ext_api; // Your new function
-};
-
 
 
 #ifdef __cplusplus
--- app/src/main.c
+++ app/src/main.c
@@ -8,13 +8,46 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/sensor.h>
-#include "our_sensor.h"
+#include <zephyr/shell/shell.h>
 
 #define BLINK_TIME_ms		500
 
 static const struct device *sensor_device = DEVICE_DT_GET_ANY(our_sensor);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+static int cmd_sensor_info(const struct shell *sh, size_t argc, char** argv){
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	shell_print(sh, "name: %s - state: %s", DEVICE_DT_NAME(	DT_NODELABEL(led_sensor) ), ((device_is_ready(sensor_device)) ? "Ready" : "Not Ready"));
+
+	return 0;
+}
+
+static int cmd_sensor_fetch(const struct shell *sh, size_t argc, char** argv){
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	shell_print(sh, "calling sensor_sample_fetch");
+
+	sensor_sample_fetch(sensor_device);
+
+	return 0;
+}
+
+static int cmd_sensor_read(const struct shell *sh, size_t argc, char** argv){
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	struct sensor_value sensor_val;
+
+	sensor_channel_get(sensor_device, 0, &sensor_val);
+
+	shell_print(sh, "sample result: %d", sensor_val.val1);
+
+	return 0;
+}
 
 int main(void)
 {
@@ -24,18 +57,19 @@ int main(void)
 
 	LOG_INF("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
-	const struct custom_sensor_api_extension *api = sensor_device->api;
-	
-	if(api->ext_api){
-		api->ext_api(sensor_device, 1024);
-	}
-
 	while(1){
-		sensor_sample_fetch(sensor_device);
-		k_msleep(BLINK_TIME_ms);
-		sensor_channel_get(sensor_device,0,NULL);
 		k_msleep(BLINK_TIME_ms);
 	}
 
 	return 0;
 }
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sensor_sub_cmds,
+	SHELL_CMD(fetch, NULL, "Calls sensor_sample_fetch", cmd_sensor_fetch),
+	SHELL_CMD(read,  NULL, "Calls sensor_channel_get",  cmd_sensor_read),
+	SHELL_CMD(info,  NULL, "Prints sensor name and state",  cmd_sensor_info),
+	SHELL_SUBCMD_SET_END
+);
+
+SHELL_CMD_REGISTER(sensor, &sensor_sub_cmds, "Sensor cmds", NULL);
